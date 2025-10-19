04a5a0ae5ed7c064b3f7de24714ceb1af8609755
Scott Carrion <scott.carrion@icloud.com>
2025-10-18 21:44:39 -0500

    Working commit toward install.sh implementation

--- install.sh
+++ install.sh
@@ -6,14 +6,14 @@ sudo cp debian_dotfiles/etc/apt/* /etc/apt
 
 echo Now installing system packages
 sudo apt-get -y update
-sudo apt-get -y install vim emacs tmux powerline btop wireshark ranger polybar make meson ninja-build cmake i3 powerline-gitstatus pipx gcc g++ python3-pip python3-cogapp libcunit1-dev libncurses-dev libncursesw-dev pkg-config libbz2-dev liblzma-dev libcunit1
+sudo apt-get -y install vim emacs tmux powerline btop wireshark ranger polybar make meson ninja-build cmake i3 powerline-gitstatus pipx gcc g++ python3-pip python3-cogapp libcunit1-dev libncurses-dev pkg-config libbz2-dev liblzma-dev libcunit1
 
 echo Now installing dotfiles
 cp debian_dotfiles/dotspacemacs ~/.spacemacs
 cp debian_dotfiles/dotbashrc ~/.bashrc
 cp debian_dotfiles/dotvimrc ~/.vimrc
 cp debian_dotfiles/dottmux.conf ~/.tmux.conf
-cp -r debian_dotfiles/dotconfig ~/.config
+cp -r debian_dotfiles/dotconfig/* ~/.config
 
 echo Now installing fonts
 sudo cp -r fonts /usr/local/share
