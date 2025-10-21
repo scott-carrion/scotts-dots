c1f6c3b71a0c4f79ab91a84e88e0fd9cb8d63772
Scott Carrion <scarrion@castelion.com>
2025-10-21 14:56:53 -0500

    Added mkdir of config dir

--- install.sh
+++ install.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 set -e
 
-echo Now installing package manager configs
-sudo cp debian_dotfiles/etc/apt/* /etc/apt
+#echo Now installing package manager configs
+#sudo cp debian_dotfiles/etc/apt/* /etc/apt
 
 echo Now installing system packages
 sudo apt-get -y update
 sudo apt-get -y install vim emacs tmux powerline btop wireshark ranger polybar make meson ninja-build cmake i3 powerline-gitstatus pipx gcc g++ python3-pip python3-cogapp libcunit1-dev libncurses-dev pkg-config libbz2-dev liblzma-dev libcunit1 kitty rofi feh libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-render-util0-dev libxcb-render0-dev libxcb-randr0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev libxcb-xinerama0-dev libxcb-glx0-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl1-mesa-dev  libpcre2-dev  libevdev-dev uthash-dev libev-dev libx11-xcb-dev xbindkeys
 
 echo Now installing dotfiles
+mkdir -p ~/.config
 cp debian_dotfiles/dotspacemacs ~/.spacemacs
 cp debian_dotfiles/dotbashrc ~/.bashrc
 cp debian_dotfiles/dotvimrc ~/.vimrc
